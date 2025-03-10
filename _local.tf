4028c74d767fff53c651283e24c090e80c301a05
Boban Ljuljdjurovic <boban@skylight.digital>
2025-03-10 04:47:13 -0400

    fix: update casing for environment variables

--- _local.tf
+++ _local.tf
@@ -67,27 +67,27 @@ locals {
           value = var.ecr_viewer_auth_pub_key
         },
         {
-          name  = "auth_azure_ad_id",
+          name  = "AUTH_AZURE_AD_ID",
           value = var.auth_azure_ad_id
         },
         {
-          name  = "auth_azure_ad_tenant_id",
+          name  = "AUTH_AZURE_AD_TENANT_ID",
           value = var.auth_azure_ad_tenant_id
         },
         {
-          name  = "secrets_manager_auth_azure_ad_secret_version",
+          name  = "AUTH_AZURE_AD_SECRET",
           value = var.secrets_manager_auth_azure_ad_secret_version
         },
         {
-          name  = "auth_keycloak_id",
+          name  = "AUTH_KEYCLOAK_ID",
           value = var.auth_keycloak_id
         },
         {
-          name  = "auth_keycloak_issuer",
+          name  = "AUTH_KEYCLOAK_ISSUER",
           value = var.auth_keycloak_issuer
         },
         {
-          name  = "secrets_manager_auth_keycloak_secret_version",
+          name  = "AUTH_KEYCLOAK_SECRET",
           value = var.secrets_manager_auth_keycloak_secret_version
         },
         local.database_url,
