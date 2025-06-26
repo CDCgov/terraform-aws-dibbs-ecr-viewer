6f4448e77d2b3f9ab1aed7529843d24dfbc9ea86
Alis Akers <alismxwll@gmail.com>
2025-06-26 14:17:00 -0700

    feat(alb): add explicit dependency on aws_s3_bucket.logging
    This ensures the ALB creation waits for the logging bucket to be ready.

--- alb.tf
+++ alb.tf
@@ -20,7 +20,8 @@ resource "aws_alb" "ecs" {
     prefix  = "connection-logs"
   }
 
-  tags = local.tags
+  tags       = local.tags
+  depends_on = [aws_s3_bucket.logging]
 }
 
 resource "aws_alb_target_group" "this" {
