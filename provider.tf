87d38b1fb2c473f401cdbc2dcc735f8015174a49
Alis Akers <alismxwll@gmail.com>
2026-01-13 11:21:36 -0800

    fix(aws): add default_tags to aws providers

--- provider.tf
+++ provider.tf
@@ -31,9 +31,15 @@ provider "dockerless" {
 
 provider "aws" {
   region = var.region
+  default_tags {
+    tags = tags
+  }
 }
 
 provider "aws" {
   alias  = "replication"
   region = var.replication_region
+  default_tags {
+    tags = tags
+  }
 }
