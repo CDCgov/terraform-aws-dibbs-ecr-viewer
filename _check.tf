eb41418732d9746eb1db8b41eecdd4ac4bef71a8
Alis Akers <alismxwll@gmail.com>
2026-01-14 08:56:45 -0800

    chore: comment out replication bucket existence check

--- _check.tf
+++ _check.tf
@@ -5,9 +5,9 @@ check "ensure_one_root_service" {
   }
 }
 
-check "ensure_replication_bucket_exists" {
-  assert {
-    condition     = var.s3_replication_bucket_name != ""
-    error_message = "S3 replication bucket name cannot be empty"
-  }
-}
+# check "ensure_replication_bucket_exists" {
+#   assert {
+#     condition     = var.s3_replication_bucket_name != ""
+#     error_message = "S3 replication bucket name cannot be empty"
+#   }
+# }
