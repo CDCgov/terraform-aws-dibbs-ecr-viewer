4378f682a2d8e3477b162e93a4c2fb5ff8160460
Alis Akers <alismxwll@gmail.com>
2026-01-27 22:31:56 -0800

    feat: add variables to control security group egress and ingress from anywhere
    The changes introduce two new boolean variables:
    - `allow_security_group_egress_from_anywhere` - controls if security group egress allows traffic from 0.0.0.0/0
    - `allow_security_group_ingress_from_anywhere` - controls if security group ingress allows traffic from 0.0.0.0/0
    
    These variables default to false, making the configuration compliant by default. When set to true, they allow unrestricted access (non-compliant). The changes update three security group rules in alb.tf to use these variables instead of hardcoded "0.0.0.0/0" CIDR blocks.
    
    The README.md file is updated with documentation for these new variables.

--- alb.tf
+++ alb.tf
@@ -241,7 +241,7 @@ resource "aws_security_group_rule" "ecs_all_egress" {
   protocol          = "-1"
   description       = "Allow outbound traffic from ECS"
   security_group_id = aws_security_group.ecs.id
-  cidr_blocks       = ["0.0.0.0/0"]
+  cidr_blocks       = var.allow_security_group_egress_from_anywhere ? ["0.0.0.0/0"] : [data.aws_route_table.this[0].cidr_block]
 }
 
 # Security Group for alb
@@ -266,7 +266,7 @@ resource "aws_security_group_rule" "alb_http_ingress" {
   protocol          = "TCP"
   description       = "Allow http inbound traffic from internet"
   security_group_id = aws_security_group.alb.id
-  cidr_blocks       = ["0.0.0.0/0"]
+  cidr_blocks       = var.allow_security_group_ingress_from_anywhere ? ["0.0.0.0/0"] : [data.aws_route_table.this[0].cidr_block]
 }
 
 # Alb Security Group Rules - INBOUND
@@ -292,5 +292,5 @@ resource "aws_security_group_rule" "alb_egress" {
   protocol          = "-1"
   description       = "Allow outbound traffic from alb"
   security_group_id = aws_security_group.alb.id
-  cidr_blocks       = ["0.0.0.0/0"]
+  cidr_blocks       = var.allow_security_group_egress_from_anywhere ? ["0.0.0.0/0"] : [data.aws_route_table.this[0].cidr_block]
 }
